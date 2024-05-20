0e1b8042e17f54c89a2b8ec0ecb9adf325dff1e5
ericssendelima <ericssendelima@gmail.com>
2024-05-20 10:12:23 -0300

    Criação do arquivo data para retirar os dados de Projects.js

---
+++ src/components/Data/data.js
@@ -0,0 +1,78 @@
+export const data = [{
+    titulo: "Meu Portfólio",
+    descricao:
+      "Site criado para facilitar minha apresantação, principais projetos e meus contatos. ",
+    descricaocompleta:
+      "Projeto desenvolvido do absoluto zero, após buscar inspirações em outros portfólios na web, porém, sem copiar nenhum componente, visando desenvolver minhas habilidades de desenvolvimento e pesquisa.",
+    tags: "React JS",
+    images: [
+      "https://i.imgur.com/nPgxtAq.jpeg",
+      "https://images.pexels.com/photos/23508826/pexels-photo-23508826/free-photo-of-flamingo-rosa.jpeg",
+      "https://i.imgur.com/BpAwYq6.jpeg",
+      "https://i.imgur.com/T83AfL1.jpeg",
+      "https://i.imgur.com/yFdaaEU.jpeg",
+    ],
+    url: "",
+  },
+  {
+    titulo: "Lista de EPI's",
+    descricao:
+      "A Lista de Epi's facilita a solicitação de EPI's em uma empresa. [Teste com matrículas: 2073 e 12345] ",
+    descricaocompleta:
+      "Sistema web desenvolvido com o objetivo de extinguir filas e o uso de papel. Cada colaborador solicita pelo seu próprio aparelho - celular ou pc - e envia sua solicitação para o whatsapp do encarregado",
+    tags: "React JS, Context API, Axios, Bootstrap",
+    images: [
+      "https://i.imgur.com/nPgxtAq.jpeg",
+      "https://i.imgur.com/S1OPVB6.jpeg",
+      "https://i.imgur.com/BpAwYq6.jpeg",
+      "https://i.imgur.com/T83AfL1.jpeg",
+      "https://i.imgur.com/yFdaaEU.jpeg",
+    ],
+    url: "https://ericssendelima.github.io/epi-lista/",
+  },
+  {
+    titulo: "Relatório de Produção",
+    descricao:
+      "Sistema para automatizar a criação do relatório diário da produção de uma empresa.",
+    descricaocompleta: "Relatório diário da produção, com envio para o whatsapp, consulta do histórico de relatórios e alteração de dados - CRUD",
+    tags: "React JS, Styled-Components, Firebase, CRUD",
+    images: [
+      "https://i.imgur.com/nPgxtAq.jpeg",
+      "https://i.imgur.com/S1OPVB6.jpeg",
+      "https://i.imgur.com/BpAwYq6.jpeg",
+      "https://i.imgur.com/T83AfL1.jpeg",
+      "https://i.imgur.com/yFdaaEU.jpeg",
+    ],
+    url: "",
+  },
+  {
+    titulo: "Catálogo de Produtos",
+    descricao:
+      "Catálogo digital de produtos de uma loja. ",
+    descricaocompleta: "Criado para facilitar o acesso de clientes aos produtos de uma loja, criar seu pedido e enviá-lo para o whatsapp do estabelecimento.",
+    tags: "React JS, React-Router-Dom, Bootstrap, Firebase",
+    images: [
+      "https://i.imgur.com/nPgxtAq.jpeg",
+      "https://i.imgur.com/S1OPVB6.jpeg",
+      "https://i.imgur.com/BpAwYq6.jpeg",
+      "https://i.imgur.com/T83AfL1.jpeg",
+      "https://i.imgur.com/yFdaaEU.jpeg",
+    ],
+    url: "https://catalogo-liras.vercel.app/",
+  },
+  {
+    titulo: "Painel de Gerenciamento",
+    descricao:
+      "Sistema para gerenciamento de dados que são consultados pelo Catálogo de Produtos",
+    descricaocompleta: "Painel de gerenciamento dos dados que são consultados pelo Catálogo de Produtos - CRUD. [A funcionalidade dos pedidos está em construção] ",
+    tags: "React JS, React-Router-Dom, Firebase, CRUD",
+    images: [
+      "https://i.imgur.com/nPgxtAq.jpeg",
+      "https://i.imgur.com/S1OPVB6.jpeg",
+      "https://i.imgur.com/BpAwYq6.jpeg",
+      "https://i.imgur.com/T83AfL1.jpeg",
+      "https://i.imgur.com/yFdaaEU.jpeg",
+    ],
+    url: "",
+  }
+]
